511ac80c9c18fcc1217d4f5b5e8cf7bb307de513
ragewolf254 <ragewolf254@yahoo.com>
2018-02-06 09:14:27 +0800

    Refixed layout contents

--- app/src/main/java/com/ragew/creativesolutions/e_cor/LoginPage.java
+++ app/src/main/java/com/ragew/creativesolutions/e_cor/LoginPage.java
@@ -10,27 +10,35 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginPage extends AppCompatActivity {
-    private EditText m_username = findViewById(R.id.usernameTextField);
-    private EditText m_password = findViewById(R.id.passwordTextField);
-    private Button m_loginButton = findViewById(R.id.loginButton);
-    private ImageView m_imageView = findViewById(R.id.parentLogo);
+    private TextView m_username;
+    private TextView m_password;
+    private Button m_loginButton;
+    private ImageView m_imageView;
     private String username;
     private String password;
-    private Intent homePage;
+    private Intent[] homePage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
-        username = m_username.getText().toString();
-        password = m_password.getText().toString();
+        m_username = findViewById(R.id.usernameTextField);
+        m_password = findViewById(R.id.passwordTextField);
+        m_loginButton = findViewById(R.id.loginButton);
+        m_imageView = findViewById(R.id.parentLogo);
+        homePage = new Intent[1];
+
         m_loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (username == "test" && password == "test"){
-                    homePage = new Intent(LoginPage.this, HomePage.class);
-                    startActivity(homePage);
+                username = m_username.getText().toString();
+                password = m_password.getText().toString();
+                if (username.equals("test") && password.equals("test")){
+                    homePage[0] = new Intent(LoginPage.this, HomePage.class);
+                    startActivity(homePage[0]);
                     finish();
                 } else {
                     Snackbar.make(m_imageView, "Login Failed", Snackbar.LENGTH_LONG).show();
--- app/src/main/java/com/ragew/creativesolutions/e_cor/TransactionHistory.java
+++ app/src/main/java/com/ragew/creativesolutions/e_cor/TransactionHistory.java
@@ -31,7 +31,7 @@ public class TransactionHistory extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_transaction_history, container, false);
-        history_listView.findViewById(R.id.historyListView);
+        history_listView = view.findViewById(R.id.historyListView);
         historyArrayList = new ArrayList<String>();
         historyArrayList.add("01/12/99");
         historyArrayList.add("01/13/99");
