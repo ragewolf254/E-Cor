14198beac81ba9494e618324f173947400561124
ragewolf254 <ragewolf254@yahoo.com>
2018-02-09 09:44:22 +0800

    Added Calendar

--- app/src/main/java/com/ragew/creativesolutions/e_cor/HomePage.java
+++ app/src/main/java/com/ragew/creativesolutions/e_cor/HomePage.java
@@ -1,5 +1,7 @@
 package com.ragew.creativesolutions.e_cor;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,18 +14,68 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class HomePage extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMMM - yyyy", Locale.getDefault());
+    CompactCalendarView compactCalendarView;
+    private ImageView m_imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        //Calendar
+        final TextView monthDisplay = findViewById(R.id.month_display);
+        compactCalendarView = findViewById(R.id.compactcalendar_view);
+        compactCalendarView.setFirstDayOfWeek(Calendar.MONDAY);
+        compactCalendarView.setUseThreeLetterAbbreviation(true);
+
+        //Do calendar shit here
+        compactCalendarView = findViewById(R.id.compactcalendar_view);
+        compactCalendarView.setFirstDayOfWeek(Calendar.MONDAY);
+        compactCalendarView.setUseThreeLetterAbbreviation(true);
+
+        final Event testEvent = new Event(Color.RED,1518153337000L, "This is the date of your event!");
+        compactCalendarView.addEvent(testEvent);
+
+        compactCalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            String dataContainer;
+            @Override
+            public void onDayClick(Date dateClicked) {
+                List<Event> events = compactCalendarView.getEvents(dateClicked);
+                for (Event x : events){
+                    x.getData();
+                    dataContainer = x.toString();
+                }
+                Toast.makeText(HomePage.this,dataContainer,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                monthDisplay.setText(simpleDateFormat.format(firstDayOfNewMonth));
+            }
+        });
+
+        m_imageView = findViewById(R.id.parentLogo);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,20 +83,22 @@ public class HomePage extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+        Snackbar.make(fab, "Login Successful!", Snackbar.LENGTH_LONG).show();
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -95,7 +149,10 @@ public class HomePage extends AppCompatActivity
         } else if (id == R.id.client_information) {
 
         } else if (id == R.id.nav_logout) {
-
+            Intent logoutIntent = new Intent(HomePage.this, LoginPage.class);
+            startActivity(logoutIntent);
+            finish();
+            Snackbar.make(m_imageView, "Logout Successful", Snackbar.LENGTH_LONG).show();
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
